cf58e71bdada17d37b76e95ceb698760b2a5f5ac
shaziabarkat5 <38329467+shaziabarkat5@users.noreply.github.com>
2018-08-02 21:24:27 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner a1= new Scanner(System.in);
+        int  a2=a1.nextInt();
+        for(int i=1;i<=a2;i++)
+        {
+           System.out.println("hello"); 
+        }
+            }
+}
